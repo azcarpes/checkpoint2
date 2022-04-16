5c68254c36e1efea332af5bdf28dd77f08b6abc0
azcarpes <azcarpes@hotmail.com>
2022-04-15 23:00:30 -0300

    aplicada funcao de remover tarefas

--- scripts/tarefas/tarefasPendentes.js
+++ scripts/tarefas/tarefasPendentes.js
@@ -3,14 +3,21 @@ let tarefasPendentes = document.querySelector(".tarefas-pendentes");
 function mostraTarefasPendentes(tarefa) {
     let liTarefasPendentes = document.createElement("li");
     liTarefasPendentes.classList.add("tarefa");
+    liTarefasPendentes.setAttribute("id", tarefa.id)
 
     liTarefasPendentes.innerHTML =
         `            
-        <div class="not-done" id="${tarefa.id}"></div>
+        <div class="not-done"></div>
         <div class="descricao">
             <p class="nome">${tarefa.description}</p>
             <p class="timestamp"><i class="far fa-calendar-alt"></i>${tarefa.createdAt}</p>
          </div>
+         <img id="lixeira" src="/ToDo-App/assets/bin.png" onclick="deletarTarefa(${tarefa.id}, ${tarefa.completed})">
     `
     tarefasPendentes.appendChild(liTarefasPendentes)
+}
+
+function capturaIdTarefa(idTarefa) {
+
+    console.log(idTarefa);
 }
--- scripts/tarefas/tarefasTerminadas.js
+++ scripts/tarefas/tarefasTerminadas.js
@@ -3,14 +3,16 @@ let tarefasTerminadas = document.querySelector(".tarefas-terminadas");
 function mostraTarefasTerminadas(tarefa) {
     let liTarefas = document.createElement("li");
     liTarefas.classList.add("tarefa");
+    liTarefas.setAttribute("id", tarefa.id)
 
     liTarefas.innerHTML =
-        `            
-        <div class="done" id="${tarefa.id}"></div>
+        `
+        <div class="done"></div>
         <div class="descricao">
             <p class="nome">${tarefa.description}</p>
             <p class="timestamp"><i class="far fa-calendar-alt"></i>${tarefa.createdAt}</p>
-         </div>
+        </div>
+        <img id="lixeira" src="/ToDo-App/assets/bin.png" onclick="deletarTarefa(${tarefa.id}, ${tarefa.completed})">
     `
     tarefasTerminadas.appendChild(liTarefas)
 }
